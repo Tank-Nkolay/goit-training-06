8582f4bc592273f5a6241df0dc895b6955cc3770
Nikolay Tankov <tankov.n.k@gmail.com>
2022-06-29 22:15:09 +0300

    push-29.06.

--- js/modal.js
+++ js/modal.js
@@ -64,23 +64,152 @@
 // console.log(author); // Bernard Cornwell
 // console.log(coverImage); // https://via.placeholder.com/640/480
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "На берегу спокойных вод",
-    author: "Роберт Шекли",
-    rating: 8.51,
-  },
-];
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "На берегу спокойных вод",
+//     author: "Роберт Шекли",
+//     rating: 8.51,
+//   },
+// ];
 
-for (const book of books) {
-  // const { title, author, rating } = book;
+// for (const book of books) {
+//   // const { title, author, rating } = book;
 
-  console.log(title);
-  console.log(author);
-  console.log(rating);
-}
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
+
+// ________________________________________________________________________
+
+// Example 4 - Комплексные задачи
+// Напиши скрипт управления личным кабинетом интернет банка. Есть объект account в котором необходимо реализовать методы для работы с балансом и историей транзакций.
+
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
+
+// /*
+//  * Каждая транзакция это объект со свойствами: id, type и amount
+//  */
+
+// const account = {
+//   // Текущий баланс счета
+//   balance: 0,
+
+//   // История транзакций
+//   transactions: [],
+
+//   /*
+//    * Метод создает и возвращает объект транзакции.
+//    * Принимает сумму и тип транзакции.
+//    */
+//   createTransaction(amount, type) {
+//     return {
+//       id: this.transactions.length,
+//       amount, // amount: amount,
+//       type, // type: type,
+//       date: Date.now(),
+//     };
+//   },
+
+//   /*
+//    * Метод отвечающий за добавление суммы к балансу.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций
+//    */
+//   deposit(amount) {
+//     this.balance += amount;
+
+//     const newTranslation = this.createTransaction(amount, Transaction.DEPOSIT);
+
+//     this.transactions.push(newTranslation);
+//   },
+
+//   /*
+//    * Метод отвечающий за снятие суммы с баланса.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций.
+//    *
+//    * Если amount больше чем текущий баланс, выводи сообщение
+//    * о том, что снятие такой суммы не возможно, недостаточно средств.
+//    */
+//   withdraw(amount) {
+//     if (amount > this.balance) {
+//       console.log('no money :(');
+//     } else {
+//       this.balance -= amount;
+
+//       const newTranslation = this.createTransaction(
+//         amount,
+//         Transaction.WITHDRAW
+//       );
+
+//       this.transactions.push(newTranslation);
+//     }
+//   },
+
+//   /*
+//    * Метод возвращает текущий баланс
+//    */
+//   getBalance() {
+//     return this.balance;
+//   },
+
+//   /*
+//    * Метод ищет и возвращает объект транзации по id
+//    */
+//   getTransactionDetails(id) {
+//     for (const item of this.transactions) {
+//       if (item.id === id) {
+//         return item;
+//       }
+//     }
+//   },
+
+//   /*
+//    * Метод возвращает количество средств
+//    * определенного типа транзакции из всей истории транзакций
+//    */
+//   getTransactionTotal(type) {
+//     let sum = 0;
+
+//     for (const item of this.transactions) {
+//       if (item.type === type) {
+//         sum += item.amount;
+//       }
+//     }
+
+//     return sum;
+//   },
+// };
+
+// console.log(account.balance); // 0
+
+// account.deposit(100);
+// account.deposit(50);
+// console.log(account.balance); // 100
+
+// account.withdraw(140);
+// account.withdraw(50);
+// console.log(account.balance); // 10
+
+// console.log(account.transactions);
+
+// console.log(account.getTransactionDetails(2));
+// console.log(account.getTransactionDetails(20));
+
+// console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+// console.log(account.getTransactionTotal(Transaction.WITHDRAW));
