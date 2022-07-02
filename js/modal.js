e50be5e660ed4202e43ba6452143fa6c40e56474
Nikolay Tankov <tankov.n.k@gmail.com>
2022-07-02 22:19:19 +0300

    111

--- js/modal.js
+++ js/modal.js
@@ -307,3 +307,38 @@
 // formatMessage("Vestibulum facilisis purus nec", 30);
 // formatMessage("Nunc sed turpis a felis in nunc fringilla", 15);
 // formatMessage("Nunc sed turpis a felis in nunc fringilla", 41);
+
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name);
+
+//   console.log(result);
+
+//   return result;
+// }
+
+// checkForName("Egor Kolbasov", "Egor");
+// checkForName("Egor Kolbasov", "egor");
+// checkForName("Egor Kolbasov", "egOr");
+// checkForName("Egor Kolbasov", "Zhenya");
+// checkForName("Vadim Nekrasov", "Vadim");
+// checkForName("Vadim Nekrasov", "vadim");
+// checkForName("Vadim Nekrasov", "Dima");
+
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length - 1];
+
+// console.log(firstElement);
+
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+
+// const lastElementIndex = ;
+
+const lastElement = fruits[fruits.length - 1];
+
+console.log(lastElement);
