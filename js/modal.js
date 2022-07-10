792429f413f5a670a7d1395506c5707239455ce3
Nikolay Tankov <tankov.n.k@gmail.com>
2022-07-10 21:45:01 +0300

    10.07.-3

--- js/modal.js
+++ js/modal.js
@@ -462,68 +462,80 @@
 //   },
 // };
 
-// atTheOldToad.addPotion("Invisibility"); // в свойстве potions будет массив ["Speed potion", "Dragon breath", "Stone skin", "Invisibility"]
+// // atTheOldToad.addPotion("Invisibility"); // в свойстве potions будет массив ["Speed potion", "Dragon breath", "Stone skin", "Invisibility"]
 
-// console.log(atTheOldToad.potions);
+// // console.log(atTheOldToad.potions);
 
-// atTheOldToad.addPotion("Power potion"); // в свойстве potions будет массив ["Speed potion", "Dragon breath", "Stone skin", "Invisibility", "Power potion"]
+// // atTheOldToad.addPotion("Power potion"); // в свойстве potions будет массив ["Speed potion", "Dragon breath", "Stone skin", "Invisibility", "Power potion"]
 
-// console.log(atTheOldToad.potions);
+// // console.log(atTheOldToad.potions);
 
-// ==========================================================================
+// // ==========================================================================
 
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(newPotion) {
-    for (const potion of this.potions) {
-      if (potion.name === newPotion.name) {
-        return `Error! Potion ${newPotion.name} is already in your inventory!`;
-      }
-    }
-    this.potions.push(newPotion);
-  },
-  removePotion(potionName) {
-    for (let i = 0; i < this.potions.length; i += 1) {
-      if (potionName === this.potions[i].name) {
-        this.potions.splice(i, 1);
-      }
-    }
-  },
-  updatePotionName(oldName, newName) {
-    for (let i = 0; i < this.potions.length; i += 1) {
-      if (oldName === this.potions[i].name) {
-        this.potions[i].name = newName;
-        return;
-      }
-    }
-    return `Potion ${oldName} is not in inventory!`;
-  },
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     for (const potion of this.potions) {
+//       if (potion.name === newPotion.name) {
+//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       }
+//     }
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(potionName) {
+//     for (let i = 0; i < this.potions.length; i += 1) {
+//       if (potionName === this.potions[i].name) {
+//         this.potions.splice(i, 1);
+//       }
+//     }
+//   },
+//   updatePotionName(oldName, newName) {
+//     for (let i = 0; i < this.potions.length; i += 1) {
+//       if (oldName === this.potions[i].name) {
+//         this.potions[i].name = newName;
+//         return;
+//       }
+//     }
+//     return `Potion ${oldName} is not in inventory!`;
+//   },
 
-  // Change code above this line
-};
+//   // Change code above this line
+// };
 
-atTheOldToad.getPotions(); // для исходного объекта возвращает [ { name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 } ]
+// atTheOldToad.getPotions(); // для исходного объекта возвращает [ { name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 } ]
 
-atTheOldToad.addPotion({ name: "Power potion", price: 270 }); // в массиве potions последним элементом будет этот объект
+// atTheOldToad.addPotion({ name: "Power potion", price: 270 }); // в массиве potions последним элементом будет этот объект
 
-atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }); // массив potions не изменяется
+// atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }); // массив potions не изменяется
 
-atTheOldToad.addPotion({ name: "Stone skin", price: 240 }); //, массив potions не изменяется
+// atTheOldToad.addPotion({ name: "Stone skin", price: 240 }); //, массив potions не изменяется
 
-atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }); // возвращает строку "Error! Potion Dragon breath is already in your inventory!"
+// atTheOldToad.addPotion({ name: "Dragon breath", price: 700 }); // возвращает строку "Error! Potion Dragon breath is already in your inventory!"
 
-atTheOldToad.removePotion("Dragon breath"); // в свойстве potions будет массив [ { name: "Speed potion", price: 460 }, { name: "Stone skin", price: 520 } ]
+// atTheOldToad.removePotion("Dragon breath"); // в свойстве potions будет массив [ { name: "Speed potion", price: 460 }, { name: "Stone skin", price: 520 } ]
 
-atTheOldToad.removePotion("Speed potion"); // в свойстве potions будет массив [ { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 }]
+// atTheOldToad.removePotion("Speed potion"); // в свойстве potions будет массив [ { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 }]
 
-atTheOldToad.updatePotionName("Dragon breath", "Polymorth"); // в свойстве potions будет массив [{ name: "Speed potion", price: 460 }, { name: "Polymorth", price: 780 }, { name: "Stone skin", price: 520 } ]
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth"); // в свойстве potions будет массив [{ name: "Speed potion", price: 460 }, { name: "Polymorth", price: 780 }, { name: "Stone skin", price: 520 } ]
 
-atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion"); // в свойстве potions будет массив [{ name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Invulnerability potion", price: 520 } ]
+// atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion"); // в свойстве potions будет массив [{ name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Invulnerability potion", price: 520 } ]
+
+const students = [
+  { name: "Манго", score: 83 },
+  { name: "Поли", score: 59 },
+  { name: "Аякс", score: 37 },
+  { name: "Киви", score: 94 },
+];
+
+const inAscendingScoreOrder = students.sort(
+  (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+);
+console.log(inAscendingScoreOrder);
