06ea1787d0204bbea971c06f186ed24d4bfd0deb
Nikolay Tankov <tankov.n.k@gmail.com>
2022-07-14 06:54:17 +0300

    14.07-8

--- js/modal.js
+++ js/modal.js
@@ -548,3 +548,13 @@ function makeMessage(callback) {
 }
 
 makeMessage(customer.getFullName); // Будет ошибка при вызове функции
+
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+const getWage = (baseSalary, overtime, rate) => {
+  return baseSalary + overtime * rate;
+};
+
+getWage(baseSalary, overtime, rate);
