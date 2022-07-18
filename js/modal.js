b3f1c5de56d395c9d8db3c40c88d53c43f4aaf29
Nikolay Tankov <tankov.n.k@gmail.com>
2022-07-18 07:20:17 +0300

    18.07.-3

--- js/modal.js
+++ js/modal.js
@@ -588,23 +588,412 @@
 // // Перебирающий forEach
 // numbers.forEach(function (number, index) {
 //   console.log(`Индекс ${index}, значение ${number}`);
-// });
+// // });
 
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
-  // Change code below this line
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
 
-  // calculateTotalPrice.forEach(function (number, index) {
-  //   console.log(`Индекс ${index}, значение ${number}`);
-  // });
-  // Change code above this line
-  // return totalPrice;
-}
+//   // calculateTotalPrice.forEach(function (number, index) {
+//   //   console.log(`Индекс ${index}, значение ${number}`);
+//   // });
+//   // Change code above this line
+//   // return totalPrice;
+// }
 
-// console.log(calculateTotalPrice[orderedItems]);
+// // console.log(calculateTotalPrice[orderedItems]);
 
-calculateTotalPrice([12, 85, 37, 4]); // возвращает 138
-calculateTotalPrice([164, 48, 291]); // возвращает 503
-calculateTotalPrice([412, 371, 94, 63, 176]); // возвращает 1116
+// calculateTotalPrice([12, 85, 37, 4]); // возвращает 138
+// calculateTotalPrice([164, 48, 291]); // возвращает 503
+// calculateTotalPrice([412, 371, 94, 63, 176]); // возвращает 1116
 
-console.log(calculateTotalPrice(orderedItems));
+// console.log(calculateTotalPrice(orderedItems));
+
+// -----------------------------------------------------------------------------
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   // Change code below this line
+
+//   numbers.forEach(function (number, index) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   console.log(filteredNumbers);
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+
+// filterArray([1, 2, 3, 4, 5], 3); // возвращает [4, 5]
+// filterArray([1, 2, 3, 4, 5], 4); // возвращает [5]
+// filterArray([1, 2, 3, 4, 5], 5); // возвращает []
+// filterArray([12, 24, 8, 41, 76], 38); // возвращает [41, 76]
+// filterArray([12, 24, 8, 41, 76], 20); // возвращает [24, 41, 76]
+
+// -----------------------------------------------------------------------------
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+//   firstArray.forEach(function (number, index) {
+//     if (secondArray.includes(number)) {
+//       commonElements.push(number);
+//     }
+//   });
+
+//   // for (let i = 0; i < firstArray.length; i += 1) {
+//   //   if (secondArray.includes(firstArray[i])) {
+//   //     commonElements.push(firstArray[i]);
+//   //   }
+//   // }
+
+//   console.log(commonElements);
+
+//   return commonElements;
+//   // Change code above this line
+// }
+
+// getCommonElements([1, 2, 3], [2, 4]); // возвращает [2]
+// getCommonElements([1, 2, 3], [2, 1, 17, 19]); // возвращает [1, 2]
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]); // возвращает [12, 27, 3]
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]); // возвращает [10, 30, 40]
+// getCommonElements([1, 2, 3], [10, 20, 30]); // возвращает []
+
+// -----------------------------------------------------------------------------
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// };
+
+// numbers.forEach(logMessage);
+
+// -----------------------------------------------------------------------------
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+
+//   const newArray = [];
+
+//   numbers.forEach((number) => {
+//     if (number % 2 === 0) {
+//       newArray.push(number + value);
+//     }
+//     if (number % 2 !== 0) {
+//       newArray.push(number);
+//     }
+//   });
+
+//   console.log(newArray);
+
+//   return newArray;
+
+//   // Change code above this line
+// }
+
+// changeEven([1, 2, 3, 4, 5], 10); // возвращает новый массив [1, 12, 3, 14, 5]
+// changeEven([2, 8, 3, 7, 4, 6], 10); // возвращает новый массив [12, 18, 3, 7, 14, 16]
+// changeEven([17, 24, 68, 31, 42], 100); // возвращает новый массив [17, 124, 168, 31, 142]
+// changeEven([44, 13, 81, 92, 36, 54], 100); // возвращает новый массив [144, 13, 81, 192, 136, 154]
+
+// -----------------------------------------------------------------------------
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map((planet) => planet.length);
+
+// console.log(planetsLengths);
+
+// -----------------------------------------------------------------------------
+
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+
+// const getUserNames = (users) => {
+//   users.map((user) => user.name);
+// };
+
+// // const getUserNames = users.map((user) => user.name);
+
+// console.log(getUserNames);
+
+// -----------------------------------------------------------------------------
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const bigValues = numbers.filter((number) => number % 2 === 0);
+// const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+
+// console.log(bigValues);
+// console.log(oddNumbers);
+
+// Значение переменной evenNumbers это массив [24, 82, 36, 18, 52]
+// Значение переменной oddNumbers это массив [17, 61, 47, 73]
+
+// -----------------------------------------------------------------------------
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+
+// const allGenres = books.flatMap((book) => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genr, index, array) => array.indexOf(genr) === index
+// );
+
+// console.log(allGenres);
+// console.log(uniqueGenres);
+
+// Значение переменной uniqueGenres это массив ["adventure", "history", "fiction", "mysticism", "horror"]
+// Для вычисления значения переменной allGenders использован метод flatMap()
+// Для вычисления значения переменной uniqueGenres использован метод filter()
+
+// -----------------------------------------------------------------------------
+
+// // Change code below this line
+// const getUsersWithEyeColor = (users, color) => {
+
+//   users.filter(
+//      if (color === users.eyeColor) {
+//      return users.name;
+//      }
+
+//   )
+//   return users;
+
+//  };
+//  // Change code above this line
+
+// -----------------------------------------------------------------------------
+
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((acc, player) => {
+//   return acc + player.name.length;
+// }, 0);
+
+// console.log(totalAveragePlaytimePerGame);
+
+// -----------------------------------------------------------------------------
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = books
+
+//   .filter((element) => element.rating > MIN_BOOK_RATING)
+//   .sort((a, b) => a.author.localeCompare(b.author))
+//   .map((book) => book.author);
+
+// console.log(names);
+
+// -----------------------------------------------------------------------------
+
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: [
+//       "Jacklyn Lucas",
+//       "Linda Chapman",
+//       "Adrian Cross",
+//       "Solomon Fokes",
+//     ],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//   },
+// ];
+
+// const getSortedFriends = (users) =>
+//   users
+//     .flatMap((user) => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .sort((a, b) => a.localeCompare(b));
+
+// console.log(getSortedFriends(users));
+
+// -----------------------------------------------------------------------------
